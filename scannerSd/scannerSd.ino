db1d1f48a85fa2df8852be0b896f0248b8b80eaa
Boris <mdeed87@gmail.com>
2015-12-01 21:20:30 +0300

    nrf+sd SPI

---
+++ scannerSd/scannerSd.ino
@@ -0,0 +1,82 @@
+#include <SPI.h>
+#include "nRF24L01.h"
+#include "RF24.h"
+
+RF24 radio(9, 10); // Для Уно
+//RF24 radio(9,53);// Для Меги
+const uint8_t num_channels = 128;
+uint8_t values[num_channels];
+
+const byte sdCS =  4; //SS
+const byte nrfCS =  10;  //CSN, NOT CE
+
+void setup(void) {
+  pinMode(sdCS, OUTPUT);
+  pinMode(nrfCS, OUTPUT);
+  digitalWrite(sdCS, 1);
+  digitalWrite(nrfCS, 1);
+
+  delay(3000);
+  Serial.begin(57600);
+  printf_begin();
+  radio.begin();
+  delay(50);
+  radio.setChannel(0);
+
+  radio.setRetries(15, 15);
+  radio.setAutoAck(false);
+  radio.setDataRate(RF24_1MBPS);
+  radio.setPALevel(RF24_PA_MAX);
+  radio.setCRCLength(RF24_CRC_8);
+
+  radio.startListening();
+
+  radio.printDetails();  // Вот эта строка напечатает нам что-то, если все правильно соединили.
+  delay(5000);              // И посмотрим на это пять секунд.
+
+  radio.stopListening();
+  int i = 0;    // А это напечатает нам заголовки всех 127 каналов
+  while ( i < num_channels )  {
+    printf("%x", i >> 4);
+    ++i;
+  }
+  printf("\n\r");
+  i = 0;
+  while ( i < num_channels ) {
+    printf("%x", i & 0xf);
+    ++i;
+  }
+  printf("\n\r");
+}
+const int num_reps = 100;
+
+void loop(void)
+{
+  memset(values, 0, sizeof(values));
+  int rep_counter = num_reps;
+  while (rep_counter--) {
+    int i = num_channels;
+    while (i--) {
+      radio.setChannel(i);
+      radio.startListening();
+      delayMicroseconds(128);
+      radio.stopListening();
+      if ( radio.testCarrier() )
+        ++values[i];
+    }
+  }
+  int i = 0;
+  while ( i < num_channels ) {
+    printf("%x", min(0xf, values[i] & 0xf));
+    ++i;
+  }
+  printf("\n\r");
+}
+int serial_putc( char c, FILE * ) {
+  Serial.write( c );
+  return c;
+}
+
+void printf_begin(void) {
+  fdevopen( &serial_putc, 0 );
+}
