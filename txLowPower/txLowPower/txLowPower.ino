8e36c2117569305fe81b0e1f0c8f489db29bd6b9
Boris <mdeed87@gmail.com>
2015-12-07 22:19:22 +0300

    poerlow

--- txLowPower/txLowPower/txLowPower.ino
+++ txLowPower/txLowPower/txLowPower.ino
@@ -7,24 +7,22 @@
 #define NRF_CE_PIN 7
 #define NRF_CSN_PIN 8
 
-uint16_t counterSleep_1s = 0;
+uint16_t counterSleep_8s = 0;
 
 void setup() {
   delay(2000);
   Serial.begin(9600);
-  NRF_init();  
+  NRF_init();
 }
 
 void loop() {
-  //LowPower.powerDown(SLEEP_1S, ADC_OFF, BOD_OFF);
-  counterSleep_1s++;
+  LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
+  counterSleep_8s++;
 
-  if (counterSleep_1s > 1) { //16 seconds
-    counterSleep_1s = 0;
+  if (counterSleep_8s > 2) {
+    counterSleep_88s = 0;
     sendDataToBase();
-  }
-
-  delay(1000);
+  }  
 }
 
 void sendDataToBase() {
