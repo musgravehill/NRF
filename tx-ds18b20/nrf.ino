f7897dd2d455da8c17c878d945a54de44fd86c41
Bob_work <musgravehill@gmail.com>
2015-12-07 11:16:15 +0400

    nrf ds18b20

---
+++ tx-ds18b20/nrf.ino
@@ -0,0 +1,76 @@
+#define NRF_CE_PIN 7
+#define NRF_CSN_PIN 8
+
+const uint8_t imSensorNum = 3; //1..5
+
+//'static' - no need
+const uint64_t pipes[6] = {
+  0xDEADBEEF00LL,  //pipe0 is SYSTEM_pipe, avoid openReadingPipe(0, );
+  0xDEADBEEF01LL,
+  0xDEADBEEF02LL,
+  0xDEADBEEF03LL,
+  0xDEADBEEF04LL,
+  0xDEADBEEF05LL
+};
+
+RF24 NRF_radio(NRF_CE_PIN, NRF_CSN_PIN);
+
+void NRF_init() {
+  Serial.print(F("Im Sensor# "));
+  Serial.print(imSensorNum);
+  Serial.print(F("\r\n"));
+
+  NRF_radio.begin();
+  delay(100);
+  NRF_radio.powerUp();
+  delay(100);
+  NRF_radio.setChannel(5);
+  NRF_radio.setRetries(15, 15);
+  NRF_radio.setDataRate(RF24_1MBPS);
+  NRF_radio.setPALevel(RF24_PA_MIN);
+  NRF_radio.setCRCLength(RF24_CRC_8);
+
+  /*
+    ===writeAckPayload===enableDynamicPayloads===
+    !  Only three of these can be pending at any time as there are only 3 FIFO buffers.
+    !  Dynamic payloads must be enabled.
+    !  write an ack payload as soon as startListening() is called
+  */
+  NRF_radio.enableDynamicPayloads();//for ALL pipes
+  //NRF_radio.setPayloadSize(32); //32 bytes? Can corrupt "writeAckPayload"?
+
+  NRF_radio.setAutoAck(true);//allow RX send answer(acknoledgement) to TX (for ALL pipes?)
+  NRF_radio.enableAckPayload(); //only for 0,1 pipes?
+  //NRF_radio.enableDynamicAck(); //for ALL pipes? Чтобы можно было вкл\выкл получение ACK?
+
+  NRF_radio.stopListening();// ?
+  NRF_radio.openWritingPipe(pipes[imSensorNum]); //pipe0 is SYSTEM_pipe, no reading
+}
+
+void NRF_sendData(uint16_t *arrayToBase) {
+  uint8_t answerFromBase; //2^8 - 1   [0,255]
+
+  //Stop listening for incoming messages, and switch to transmit mode.
+  //Do this before calling write().
+  NRF_radio.stopListening();
+  NRF_radio.write( &arrayToBase, sizeof(arrayToBase));
+
+  Serial.print(F("V= "));
+  Serial.print(arrayToBase[0]);
+  Serial.print(F("\r\n"));
+  Serial.print(F("t= "));
+  Serial.print(arrayToBase[1]);
+  Serial.print(F("\r\n"));
+  Serial.print(F("h= "));
+  Serial.print(arrayToBase[2]);
+  Serial.print(F("\r\n"));
+
+  if ( NRF_radio.isAckPayloadAvailable() ) {
+    NRF_radio.read(&answerFromBase, sizeof(answerFromBase)); //приемник принял и ответил
+
+    Serial.print(F("__Received answer from Base: "));
+    Serial.print(answerFromBase, DEC);
+    Serial.print(F("\r\n"));
+  }
+
+}
--- tx-ds18b20/tx-ds18b20.ino
+++ tx-ds18b20/tx-ds18b20.ino
@@ -3,99 +3,32 @@
 #include <RF24.h>
 #include <stdint.h>
 
-#define CE_PIN 7
-#define CSN_PIN 8
-
-const uint8_t imSensorNum = 3; //1..5
-
-//'static' - no need
-const uint64_t pipes[6] = {
-  0xDEADBEEF00LL,  //pipe0 is SYSTEM_pipe, avoid openReadingPipe(0, );
-  0xDEADBEEF01LL,
-  0xDEADBEEF02LL,
-  0xDEADBEEF03LL,
-  0xDEADBEEF04LL,
-  0xDEADBEEF05LL
-};
-
-RF24 radio(CE_PIN, CSN_PIN);
-
 void setup() {
   DS18B20_init();
 
   delay(2000);
   Serial.begin(9600);
   delay(100);
-  Serial.print(F("Im Sensor# "));
-  Serial.print(imSensorNum);
-  Serial.print(F("\r\n"));
 
-  radio.begin();
-  delay(100);
-  radio.powerUp();
-  delay(100);
-  radio.setChannel(5);
-  radio.setRetries(15, 15);
-  radio.setDataRate(RF24_1MBPS);
-  radio.setPALevel(RF24_PA_MIN);
-  radio.setCRCLength(RF24_CRC_8);
-
-
-  /*
-    ===writeAckPayload===enableDynamicPayloads===
-    !  Only three of these can be pending at any time as there are only 3 FIFO buffers.
-    !  Dynamic payloads must be enabled.
-    !  write an ack payload as soon as startListening() is called
-  */
-  radio.enableDynamicPayloads();//for ALL pipes
-  //radio.setPayloadSize(32); //32 bytes? Can corrupt "writeAckPayload"?
-
-  radio.setAutoAck(true);//allow RX send answer(acknoledgement) to TX (for ALL pipes?)
-  radio.enableAckPayload(); //only for 0,1 pipes?
-  //radio.enableDynamicAck(); //for ALL pipes? Чтобы можно было вкл\выкл получение ACK?
-
-  radio.stopListening();// ?
-  radio.openWritingPipe(pipes[imSensorNum]); //pipe0 is SYSTEM_pipe, no reading
+  NRF_init();
 }
 
-void loop()
-{
-  uint16_t messageToBase_batteryVoltage = random(0, 65535); //2^16 - 1
-  uint16_t messageToBase_temperature = getTemperatureNormalized(); // random(0, 65535); //2^16 - 1
-  uint16_t messageToBase_humidity = random(0, 65535); //2^16 - 1
+void loop() {
+  uint16_t arrayToBase_batteryVoltage = random(0, 65535); //2^16 - 1
+  uint16_t arrayToBase_temperature = getTemperatureNormalized(); // random(0, 65535); //2^16 - 1
+  uint16_t arrayToBase_humidity = random(0, 65535); //2^16 - 1
 
-  uint16_t messageToBase[3] = {
-    messageToBase_batteryVoltage,
-    messageToBase_temperature,
-    messageToBase_humidity
+  uint16_t arrayToBase[3] = {
+    arrayToBase_batteryVoltage,
+    arrayToBase_temperature,
+    arrayToBase_humidity
   };
 
-  uint8_t answerFromBase; //2^8 - 1   [0,255]
-
-  //Stop listening for incoming messages, and switch to transmit mode.
-  //Do this before calling write().
-  radio.stopListening();
-  radio.write( &messageToBase, sizeof(messageToBase));
-
-  Serial.print(F("V= "));
-  Serial.print(messageToBase[0]);
-  Serial.print(F("\r\n"));
-  Serial.print(F("t= "));
-  Serial.print(messageToBase[1]);
-  Serial.print(F("\r\n"));
-  Serial.print(F("h= "));
-  Serial.print(messageToBase[2]);
-  Serial.print(F("\r\n"));
-
-  if ( radio.isAckPayloadAvailable() ) {
-    radio.read(&answerFromBase, sizeof(answerFromBase)); //приемник принял и ответил
-
-    Serial.print(F("__Received answer from Base: "));
-    Serial.print(answerFromBase, DEC);
-    Serial.print(F("\r\n"));
-  }
+  //NRF_sendData(arrayToBase);
   delay(1000);
 }
+
+
 
 
 
